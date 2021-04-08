8348046a47d2f153ddaaec15b2c8f7b1aabe197b
shobhitkumarshaw <56765072+shobhitkumarshaw@users.noreply.github.com>
2021-04-09 00:18:48 +0530

    Changes made to admin & castedVote

--- src/main/java/org/society/entities/CastedVote.java
+++ src/main/java/org/society/entities/CastedVote.java
@@ -1,5 +1,38 @@
 package org.society.entities;
 
-public class CastedVote {
+import java.io.Serializable;
+import java.util.Map;
+/*
+ * @author Shobhit Kumar Shaw
+ * 
+ * Class Description: 
+ *
+ * 
+ */
+
+
+public class CastedVote implements Serializable {
+	
+	
+	private static final long serialVersionUID = 3L;
+	private Map<NominatedCandidates, Integer> castedVotes;
+	
+	public CastedVote() {
+		super();
+	}
+	public Map<NominatedCandidates, Integer> getCastedVotes() {
+		return castedVotes;
+	}
+	public void setCastedVotes(Map<NominatedCandidates, Integer> castedVotes) {
+		this.castedVotes = castedVotes;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+	
+	
+	
  
 }
