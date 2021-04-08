a218ea2599853aa889c87d539614f8e6fc0747c0
RitikKumarKumar <81379844+RitikKumarKumar@users.noreply.github.com>
2021-04-09 00:53:09 +0530

    Add repository

---
+++ src/main/java/org/society/repository/ElectionResultRepository.java
@@ -0,0 +1,9 @@
+package org.society.repository;
+
+import org.society.entities.ElectionResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface ElectionResultRepository extends JpaRepository<ElectionResult, Long> {
+
+}
