b4f2544358b63dd92350ddcf88fc79229ae5df6f
RitikKumarKumar <81379844+RitikKumarKumar@users.noreply.github.com>
2021-04-10 16:51:19 +0530

    change in controller of result

--- src/main/java/org/society/controller/ElectionResultController.java
+++ src/main/java/org/society/controller/ElectionResultController.java
@@ -1,5 +1,25 @@
 package org.society.controller;
 
+import javax.validation.Valid;
+
+import org.society.entities.ElectionResult;
+import org.society.service.ElectionResultService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ElectionResultController {
+	@Autowired
+	ElectionResultService service;
+
+	@PostMapping // (consumes = { MediaType.APPLICATION_XML_VALUE,
+					// MediaType.APPLICATION_JSON_VALUE })
+	public String saveElectionResult(@Valid @RequestBody ElectionResult result) {
+		service.addElectionResult(result);
+		return "Election Result successfully saved";
+	}
 
 }
