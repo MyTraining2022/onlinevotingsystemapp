441301ae32d7768bb2abe72611d7201a9673602f
RitikKumarKumar <81379844+RitikKumarKumar@users.noreply.github.com>
2021-04-11 22:05:57 +0530

    data in dbinit

---
+++ src/test/java/org/society/test/service/ElectionResultTest.java
@@ -0,0 +1,32 @@
+package org.society.test.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+import org.society.dao.ElectionResultDao;
+import org.society.entities.ElectionResult;
+import org.society.repository.ElectionResultRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
+public class ElectionResultTest {
+	@Autowired
+	private ElectionResultDao erservice;
+
+	@MockBean
+	private ElectionResultRepository repo;
+
+	@Test
+	public void addElectionResultDetailsTest() {
+	
+		ElectionResult er = new ElectionResult(121l, LocalDate.of(2021, 01, 26), null, "Saran",1000, 500, 50f, 300, 30f, "win");
+		when(repo.save(er)).thenReturn(er);
+		assertEquals(er, erservice.save(er));
+
+	}
+}
