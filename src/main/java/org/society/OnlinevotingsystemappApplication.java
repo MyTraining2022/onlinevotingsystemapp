38b2a8e3ec2a3092024c6e15389ea5816a22555e
shobhitkumarshaw <56765072+shobhitkumarshaw@users.noreply.github.com>
2021-04-12 00:24:45 +0530

    fixed bug

--- src/main/java/org/society/OnlinevotingsystemappApplication.java
+++ src/main/java/org/society/OnlinevotingsystemappApplication.java
@@ -2,8 +2,17 @@ package org.society;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class OnlinevotingsystemappApplication {
 
 	public static void main(String[] args) {
@@ -11,5 +20,21 @@ public class OnlinevotingsystemappApplication {
 
 
 	}
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				// .apis(RequestHandlerSelectors.basePackage("com.jpa"))
+				.paths(PathSelectors.any()).build().apiInfo(metaInfo());
+
+	}
+
+	private ApiInfo metaInfo() {
+
+		ApiInfo apiInfo = new ApiInfo("Sping Boot Test", "CapGemini training example", "1.0", "", "shobhit kumar shaw ",
+				"Licence 2.1.0", "https://www.cg.com");
+
+		return apiInfo;
+	}
+
 
 }
--- src/main/java/org/society/controller/ElectionOfficerController.java
+++ src/main/java/org/society/controller/ElectionOfficerController.java
@@ -1,5 +1,26 @@
 package org.society.controller;
 
-public class ElectionOfficerController {
+import org.society.entities.ElectionOfficer;
+import org.society.service.ElectionOfficerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("ElectionOfficer")
+public class ElectionOfficerController {
+	
+	@Autowired
+	ElectionOfficerService service;
+	
+	@GetMapping(value = "{id}")
+	public ResponseEntity<?> getElectionOfficerById(@PathVariable("id") long id) {
+		ElectionOfficer officer = service.viewElectionOfficerById(id);
+
+		return new ResponseEntity<ElectionOfficer>(officer, HttpStatus.OK);
+	}
 }
